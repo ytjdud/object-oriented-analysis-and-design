4df95f1d11d4e2c8d99b08e4472e3169f34e5972
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-05-21 15:55:36 +0900

    set models

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # quiz/apps.py
+    'quiz.apps.QuizConfig',
 ]
 
 MIDDLEWARE = [
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # CASCADE : ForeignKey(Question)가 삭제되면 이 data(Ansewer)도 사라진다
+    content = models.TextField()
+    create_date = models.DateTimeField()
