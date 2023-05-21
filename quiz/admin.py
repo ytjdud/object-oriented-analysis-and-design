3cebacdbe007e6fa4353e21a4786fbba1b867100
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-05-21 16:09:40 +0900

    set admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+    list_display = ["subject", "create_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
