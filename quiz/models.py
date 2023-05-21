7842cfaec7df2b3575dd8ce1bfd0b8ebaf0305eb
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-05-21 23:17:12 +0900

    글과 댓글에 글쓴이 표시 추가

--- quiz/models.py
+++ quiz/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
 
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # CASCADE : ForeignKey(Question)가 삭제되면 이 data(Ansewer)도 사라진다
     content = models.TextField()
     create_date = models.DateTimeField()
--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
-from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -21,32 +21,38 @@ def detail(request, question_id):
     return render(request, 'quiz/question_detail.html', context)
 
 
+# 로그아웃 상태에서 @login_required 어노테이션이 적용된 함수가 호출되면 자동으로 로그인 화면으로 이동하게 된다.
+# @login_required 어노테이션은 login_url='common:login' 처럼 로그인 URL을 지정할 수 있다.
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장, request.user는 현재 로그인한 계정의 User 모델 객체이다.
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('quiz:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'quiz/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
-            question = form.save(commit=False) # 임시 저장하여 question 객체를 리턴받는다. 아직 create_date 에 대한 정보가 없어서 실제 DB 에 저장할 수 없기 때문.
-            question.create_date = timezone.now() # 실제 저장을 위해 작성일시를 설정한다.
-            question.save() # 데이터를 실제로 저장한다.
+            question = form.save(
+                commit=False)  # 임시 저장하여 question 객체를 리턴받는다. 아직 create_date 에 대한 정보가 없어서 실제 DB 에 저장할 수 없기 때문.
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
+            question.save()  # 데이터를 실제로 저장한다.
             return redirect('quiz:index')
     else:
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'quiz/question_form.html', context)
-
